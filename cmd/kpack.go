1fd131a4625e0b47ac7d2d1d36fc8a5b9202e5d1
Emily Johnson <emjohnson@vmware.com>
2022-11-07 11:34:08 -0600

    WIP - test cluster builder

--- cmd/kpack.go
+++ cmd/kpack.go
@@ -50,7 +50,7 @@ func NewKpackCmd() *cobra.Command {
 	_ = kpackCmd.MarkFlagRequired("tag")
 	kpackCmd.Flags().StringVarP(&c.Env, "env-vars", "e", "", "list of build time environment variables")
 	kpackCmd.Flags().StringVarP(&c.ServiceAccountName, "service-account-name", "a", "default", "service account name that will be used for credential lookup")
-	kpackCmd.Flags().StringVarP(&c.ServiceAccountName, "cluster-builder", "b", "default", "cluster builder to use for the build")
+	kpackCmd.Flags().StringVarP(&c.ClusterBuilderName, "cluster-builder", "b", "default", "cluster builder to use for the build")
 
 	kpackCmd.Flags().StringVarP(&c.ActionOutput, "github-action-output", "o", "", "location to store output of the build")
 	_ = kpackCmd.MarkFlagRequired("github-action-output")
